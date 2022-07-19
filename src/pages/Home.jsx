2fb2ad35c2644862c5751969a598cd383cba7a8b
muaz <muaz.vakitsayan@gmail.com>
2022-07-19 11:18:18 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import data from "../data";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-const Home = () => {
+
+export default function ResponsiveGrid() {
   return (
-    <div>Home</div>
-  )
+    <Container sx={{mt:"2rem"}}>
+      <Box>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {data.map((fe) => {
+            const { id, name, text, img } = fe;
+            return (
+              <Grid item xs={12} sm={4} md={4} key={id} sx={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column"}}>
+                <Card sx={{ maxWidth: 300, textAlign:"center"}}>
+                  <CardMedia component="img" image={img} alt={name} />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {text}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Box>
+    </Container>
+  );
 }
-
-export default Home
